68998ee5cc2f88f1bf7bad766b30976557d0cc55
Praron <koctr123@mail.ru>
2018-05-02 19:10:38 +0300

    Category recycler adapter not works for now.

--- app/src/main/java/com/example/lumberjacks/moneysaver/Category.kt
+++ app/src/main/java/com/example/lumberjacks/moneysaver/Category.kt
@@ -1,3 +1,27 @@
 package com.example.lumberjacks.moneysaver
 
+import android.os.Parcel
+import android.os.Parcelable
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
 data class Category (val name: String, val description : String = "")
+
+
+class CategoryRecyclerAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    }
+
+    override fun onBindViewHolder(holder:, position: Int) {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): {
+    }
+
+    override fun getItemCount(): Int {
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+}
--- app/src/main/java/com/example/lumberjacks/moneysaver/SecondStepFragment.kt
+++ app/src/main/java/com/example/lumberjacks/moneysaver/SecondStepFragment.kt
@@ -2,13 +2,11 @@ package com.example.lumberjacks.moneysaver
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import kotlinx.android.synthetic.main.first_step_fragment.*
 import kotlinx.android.synthetic.main.second_step_fragment.*
-import android.support.v7.widget.LinearLayoutManager
 
 class SecondStepFragment : Fragment() {
 
@@ -19,12 +17,12 @@ class SecondStepFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        onWhatText.text = arguments.getInt("price").toString()
+        onWhatText.text = arguments!!.getInt("price").toString()
 
 //        val adapter = ArrayAdapter(activity, R.layout.categories_list_item, arrayOf("Food", "Clothes", "Other"))
 //        categoriesList.adapter = adapter
         val linearLayoutManager = LinearLayoutManager(activity)
-//        categoriesRecyclerView.layoutManager = linearLayoutManager
+        categoriesRecyclerView.layoutManager = linearLayoutManager
 
     }
 }
