0fc868b0fb60403c6f39b4bbd89e18a40004806c
Praron <koctr123@mail.ru>
2018-05-02 22:10:47 +0300

    Implemented category adapter

--- app/src/main/java/com/example/lumberjacks/moneysaver/Category.kt
+++ app/src/main/java/com/example/lumberjacks/moneysaver/Category.kt
@@ -1,27 +1,33 @@
 package com.example.lumberjacks.moneysaver
 
-import android.os.Parcel
-import android.os.Parcelable
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.categories_list_item.view.*
 
 data class Category (val name: String, val description : String = "")
 
 
-class CategoryRecyclerAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+class CategoryRecyclerAdapter(val categories: List<Category>, val categoryClick: (Category) -> Unit) : RecyclerView.Adapter<CategoryRecyclerAdapter.CategoryViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryRecyclerAdapter.CategoryViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.categories_list_item, parent, false)
+        return CategoryViewHolder(view, categoryClick)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        holder.bindCategory(categories[position])
     }
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun getItemCount(): Int = categories.size
+
+    inner class CategoryViewHolder(itemView: View, itemClick: (Category) -> Unit) : RecyclerView.ViewHolder(itemView) {
+        fun bindCategory(category: Category) {
+            with(category) {
+                itemView.setOnClickListener { categoryClick(this) }
+                itemView.category_name.text = name
+                itemView.category_description.text = description
+            }
+        }
     }
-
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
-
 }
