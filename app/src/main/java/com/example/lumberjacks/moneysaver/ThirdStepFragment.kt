971c3d097a93ab8e0512d6e470a0e41777698c96
Praron <koctr123@mail.ru>
2018-05-07 02:41:39 +0300

    Third screen.

--- app/src/main/java/com/example/lumberjacks/moneysaver/ThirdStepFragment.kt
+++ app/src/main/java/com/example/lumberjacks/moneysaver/ThirdStepFragment.kt
@@ -3,20 +3,44 @@ package com.example.lumberjacks.moneysaver
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v7.app.AlertDialog
-import android.support.v7.widget.DividerItemDecoration
-import android.support.v7.widget.LinearLayoutManager
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
+import kotlinx.android.synthetic.main.third_step_fragment.view.*
+import android.widget.ArrayAdapter
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.top_spending_list_item.view.*
+
 
 class ThirdStepFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.third_step_fragment, container, false)
+        return inflater.inflate(R.layout.third_step_fragment, container, false).apply {
+            val adapter = ArrayAdapter(activity, R.layout.spinner_item,
+                    arrayListOf("this day", "this week", "this month", "this year"))
+            adapter.setDropDownViewResource(R.layout.spinner_item)
+            total_spent_spinner.adapter = adapter
+            total_spent_spinner.setSelection(2)
+
+            val data = arrayListOf(Pair("Food", 3200), Pair("Clothes", 1100), Pair("Electronics", 500), Pair("Other", 30))
+            top_spends_list.adapter = TopSpentAdapter(inflater, data)
+        }
+    }
+
+    class TopSpentAdapter(private val inflater : LayoutInflater, private val data: ArrayList<Pair<String, Int>>) : BaseAdapter() {
+        override fun getItemId(position: Int): Long = position.toLong()
+
+        override fun getCount(): Int = data.size
+
+        override fun getItem(position: Int): Any = data[position]
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            return inflater.inflate(R.layout.top_spending_list_item, parent, false).apply {
+                this.category_name.text = data[position].first
+                this.category_total_spent.text = data[position].second.toString()
+            }
+        }
+
     }
 
 }
